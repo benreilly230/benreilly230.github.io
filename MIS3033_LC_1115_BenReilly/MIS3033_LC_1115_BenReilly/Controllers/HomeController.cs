30ed016ae559281c8620da698fc340df16503f16
Ben <122949830+benreilly230@users.noreply.github.com>
2023-11-15 13:11:52 -0600

    lectureprep 1115

---
+++ MIS3033_LC_1115_BenReilly/MIS3033_LC_1115_BenReilly/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MIS3033_LC_1115_BenReilly.Data;
+
+namespace MIS3033_LC_1115_BenReilly.Controllers
+{
+    public class HomeController : Controller
+    {
+        StuDB db = new StuDB();
+
+        public JsonResult GetStudents() 
+        {
+        return Json(db.Students);
+        }
+        public JsonResult GetCourses()
+        {
+            return Json(db.Courses);
+        }
+        public JsonResult GetEnrollments()
+        {
+            return Json(db.Enrollments);
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MIS3033_LC_1115_BenReilly/MIS3033_LC_1115_BenReilly/Migrations/20231115185944_createdb3.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MIS3033_LC_1115_BenReilly.Migrations
+{
+    /// <inheritdoc />
+    public partial class createdb3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "StudentId",
+                table: "Profiles",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Profiles_StudentId",
+                table: "Profiles",
+                column: "StudentId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Profiles_Students_StudentId",
+                table: "Profiles",
+                column: "StudentId",
+                principalTable: "Students",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Profiles_Students_StudentId",
+                table: "Profiles");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Profiles_StudentId",
+                table: "Profiles");
+
+            migrationBuilder.DropColumn(
+                name: "StudentId",
+                table: "Profiles");
+        }
+    }
+}
