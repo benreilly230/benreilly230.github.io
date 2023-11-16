a5a0450ed13b5776d0c5a0c1c20a951d466370fd
Ben <122949830+benreilly230@users.noreply.github.com>
2023-11-15 20:22:40 -0600

    post lec 1115

--- MIS3033_LC_1115_BenReilly/MIS3033_LC_1115_BenReilly/Controllers/HomeController.cs
+++ MIS3033_LC_1115_BenReilly/MIS3033_LC_1115_BenReilly/Controllers/HomeController.cs
@@ -7,6 +7,27 @@ namespace MIS3033_LC_1115_BenReilly.Controllers
     {
         StuDB db = new StuDB();
 
+
+        public JsonResult GD() // action function, webapi
+        {
+            // var r = db.Enrollments;
+            //var r = db.Enrollments.Include(x => x.Student).Include(x => x.Course); //
+            //var r = db.Enrollments.Select(( Enrollment a) => new {G = a.Grade, LG =a.LetterGrade  });//
+            //var r = db.Enrollments.Select(( Enrollment a) => new {G = a.Grade, LG =a.LetterGrade  }).Where(a=>a.LG == 'A').Where(a=>a.G>95);//
+            //var r = db.Enrollments.Select((Enrollment a) => new { G = a.Grade, LG = a.LetterGrade }).Where(a => a.LG == 'A' && a.G > 95);//
+
+            // var r = db.Enrollments.OrderBy(x=>x.Grade);
+            // var r = db.Enrollments.OrderByDescending(x=>x.Grade);
+
+            //var r = db.Enrollments.ToList().MaxBy(x => x.Grade); // you will get a double value
+
+
+            var r = db.Enrollments.GroupBy(x => x.LetterGrade).Select(x => new { LG = x.Key, N = x.Count(), Average = x.Average(a => a.Grade), }).OrderBy(x => x.LG);
+
+
+            return Json(r);
+        }
+
         public JsonResult GetStudents() 
         {
         return Json(db.Students);
