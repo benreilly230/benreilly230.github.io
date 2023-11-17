1cd974b66f01856478611d8f7c93bc2cb6e24f8a
Ben <122949830+benreilly230@users.noreply.github.com>
2023-11-17 13:46:55 -0600

    remove
    remove

--- MIS3033_LC_1113_BenReilly/MIS3033_LC_1108_BenReilly/Models/Message.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MIS3033_LC_1108_BenReilly.Models
-{
-    public class Message
-    {
-        public string status { get; set; } = "success";
-        public string message { get; set; } = "";
-
-    }
-}
